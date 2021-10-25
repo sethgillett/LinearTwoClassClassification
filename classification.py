c5587cb820198764d1fe83438b206cbc30863739
Seth Gillett <seth.gillett@pm.me>
2021-10-25 18:22:24 -0400

    Got working algorithms and test data

--- classification.py
+++ classification.py
@@ -14,12 +14,12 @@ from time import perf_counter
 t = 3
 # size of training data set
 data_set_size = 100
-# number of gradient descent iterations
-grad_descent_iterations = 200
-# number of times data will be generated and tested
-iterations = 3
+# maximum number of gradient descent iterations before failure
+max_grad_descent_iterations = 100
+# number of times test data will be generated and tested against the algorithms
+test_data_iterations = 100
 # whether to show plots of data and best fit lines
-show_plots = True
+show_plots = False
 
 
 # Debug function to check whether data has been properly relabeled
@@ -55,6 +55,7 @@ def soft_max_loss(w, data):
     total = 0
     for (x, y) in data:
         total += np.log(1 + np.e ** (-y * np.dot(x, w)))
+    return total / len(data)
 
 
 # Logistic step function
@@ -93,12 +94,12 @@ def gen_data(labels=(-1, 1), source_vector=None):
     return source_vector, data
 
 
-def logistic_regression(loss_function, data):
-    w = np.zeros(len(data[0][0]))
-    loss_grad = grad(loss_function)
-    for _ in range(grad_descent_iterations):
-        grad_neg = loss_grad(w)
-        print(grad_neg)
+# def logistic_regression(loss_function, data):
+#     w = np.zeros(len(data[0][0]))
+#     loss_grad = grad(loss_function)
+#     for _ in range(grad_descent_iterations):
+#         grad_neg = loss_grad(w)
+#         print(grad_neg)
 
 
 def perceptron_learning(data):
@@ -140,6 +141,15 @@ def check_result(data, w):
     return misclassifications
 
 
+# Algorithm performs significantly faster when checking with this function
+def no_misclassifications(data, w):
+    for (x, y) in data:
+        classification = math.copysign(y, np.dot(x, w))
+        if classification != y:
+            return False
+    return True
+
+
 def linprog_classifier(data):
     c = np.zeros(len(data[0][0]))
     A_ub = []
@@ -160,28 +170,32 @@ def linprog_classifier(data):
     return result
 
 
-def gradient_descent(loss_function, data):
+def gradient_descent(loss_function, data, relabel=True):
     # Changes the labels on the data to (0, 1)
-    zero_one_data = [(x, (y+1)/2) for (x, y) in data]
-    assert check_relabeling(data, zero_one_data, (-1, 1), (0, 1))
-    # Creates lambda function for use in autograd
-    f = lambda w: loss_function(w, zero_one_data)
+    if relabel:
+        zero_one_data = [(x, (y+1)/2) for (x, y) in data]
+        assert check_relabeling(data, zero_one_data, (-1, 1), (0, 1))
+        # Creates lambda function for use in autograd
+        f = lambda w: loss_function(w, zero_one_data)
+    else:
+        f = lambda w: loss_function(w, data)
     dfdx = grad(f)
     # Starts w at 0
     w = np.zeros(len(data[0][0]))
-    count = 10
+    count = 0
 
     start = perf_counter()
 
-    while check_result(data, w) > 0:
+    while not no_misclassifications(data, w) and count < max_grad_descent_iterations:
         # Adjusts the alpha to be smaller as the program progresses
-        alpha = 1 / math.log(count, 10.0)
+        alpha = 1 / (count + 1)
         slope = dfdx(w)
         w += - alpha * slope
         count += 1
 
     stop = perf_counter()
-    print(f"Gradient descent took {stop - start} seconds and {count - 10} iterations")
+    print(f"Gradient descent took {stop - start} seconds, {count} iterations, and {check_result(data, w)} "
+          f"misclassifications in training data")
 
     return w
 
@@ -222,32 +236,55 @@ def plot_results(data, vectors, labels, title):
 
 
 def classify():
-    # Perceptron Learning successes
-    pl_successes = 0
-    # Linear Program Classification successes
-    lc_successes = 0
-    # Gradient Descent successes
-    gd_successes = 0
-    for _ in range(iterations):
-        source_vector, training_data = gen_data(labels=(-1, 1))
+    # Perceptron Learning misclassifications
+    pl_misclassifications = 0
+    # Linear Program Classification misclassifications
+    lc_misclassifications = 0
+    # Gradient Descent with least squares loss function misclassifications
+    gd_ls_misclassifications = 0
+    # Gradient Descent with soft max loss function misclassifications
+    gd_sm_misclassifications = 0
+    # Gradient Descent with cross-entropy loss function misclassifications
+    gd_ce_misclassifications = 0
+    # Generate training data and source vector
+    source_vector, training_data = gen_data(labels=(-1, 1))
+    # Linprog classifier
+    print("Starting linear programming classifier")
+    lc_res = linprog_classifier(training_data).x
+    # Perceptron learning classifier
+    print("Starting perceptron learning classifier")
+    pl_res = perceptron_learning(training_data)
+    # Gradient descent with least squares loss function
+    print("Starting gradient descent with least squares loss function")
+    gd_ls_res = gradient_descent(least_squares_loss, training_data, relabel=True)
+    # Gradient descent with soft max loss function
+    print("Starting gradient descent with soft max loss function")
+    gd_sm_res = gradient_descent(soft_max_loss, training_data, relabel=False)
+    # Gradient descent with cross-entropy loss function
+    print("Starting gradient descent with cross entropy loss function")
+    gd_ce_res = gradient_descent(cross_entropy_loss, training_data, relabel=True)
+    # Show plot at end
+    titles = [
+                 "Source Vector",
+                 "Linear Classification",
+                 "Perceptron Learning",
+                 "Gradient Descent w/ Least Squares",
+                 "Gradient Descent w/ Soft Max",
+                 "Gradient Descent w/ Cross-Entropy"
+             ],
+    if show_plots:
+        plot_results(training_data, [source_vector, lc_res, pl_res, gd_ls_res, gd_sm_res, gd_ce_res],
+                     titles,
+                     "Vectors Found")
+    for _ in range(test_data_iterations):
+        # Generate test data with the same source vector
         _, test_data = gen_data(labels=(-1, 1), source_vector=source_vector)
-        # Linprog classifier
-        lc_res = linprog_classifier(training_data)
-        print(f"LP classifier misclassified test data {check_result(test_data, lc_res.x)} times")
-        # Perceptron learning classifier
-        pl_res = perceptron_learning(training_data)
-        print(f"LP classifier misclassified test data {check_result(test_data, pl_res)} times")
-        # Gradient descent
-        gd_res = gradient_descent(least_squares_loss, training_data)
-        print(f"Gradient descent misclassified test data {check_result(test_data, gd_res)} times")
-        # Show plot at end
-        if show_plots:
-            plot_results(test_data, [source_vector, lc_res.x, pl_res, gd_res],
-                     ["Source Vector",
-                      "Linear Classification",
-                      "Perceptron Learning",
-                      "Gradient Descent"],
-                         "Vectors")
+        lc_misclassifications += check_result(test_data, lc_res)
+        pl_misclassifications += check_result(test_data, pl_res)
+        gd_ls_misclassifications += check_result(test_data, gd_ls_res)
+        gd_sm_misclassifications += check_result(test_data, gd_sm_res)
+        gd_ce_misclassifications += check_result(test_data, gd_ce_res)
+    print()
 
 
 classify()
